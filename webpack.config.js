0a77aebec7e1238cabe7180f8d8aad3b9043df5d
anyoneout <chrisdafur@gmail.com>
2025-03-20 17:30:56 -0500

    sorted dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,32 @@
 import Dotenv from "dotenv-webpack";
 
-
 export default {
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)/,
-                use: "babel-loader",
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.(scss|css)/,
-                use: ["style-loader", "css-loader", "sass-loader"],
-            },
-            {
-                test: /\.(jpg|png|mp4)/,
-                type: "asset/resource",
-            },
-        ],
-    },
-    watch: true,
-    mode: "development",
-    devtool: "source-map",
-    plugins: [
-        new Dotenv({
-            systemvars: true,
-        })
-    ]
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(scss|css)/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(jpg|png|mp4)/,
+        type: "asset/resource",
+      },
+    ],
+  },
+  watch: true,
+  mode: "development",
+  devtool: "source-map",
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
+  plugins: [
+    new Dotenv({
+      systemvars: true,
+    }),
+  ],
 };
