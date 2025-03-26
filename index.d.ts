c206e8798e2434ae860f6773839137c74a669c9f
anyoneout <chrisdafur@gmail.com>
2025-03-26 17:56:29 -0500

    copied files to frontend folder. not sure if this is correct so didn't want to move them an possible mess things up

--- index.d.ts
+++ index.d.ts
@@ -2,4 +2,3 @@ declare module "*.jpg";
 declare module "*.mp4";
 declare module "*.png";
 // added to fix tooltip typescript issues until @types/bootstrap is installed and imported into Home.tsx or a better solution is taught
-declare const bootstrap: any;
