48e3df46be4987e82ebb4d4c722e6f4c18e2b87b
anyoneout <chrisdafur@gmail.com>
2025-04-05 00:11:54 -0500

    added h2 title to backend message

--- frontend/src/Views/DynamoAuthPage.tsx
+++ frontend/src/Views/DynamoAuthPage.tsx
@@ -3,15 +3,14 @@ import React, { useEffect, useState } from "react";
 
 export function DynamoAuthPage() {
   const [authResponse, setAuthResponse] = useState<boolean>(null);
+  const [notReachable, setNotReachable] = useState<boolean>(null);
 
   function componentDidMount() {
     getAuthResponse();
   }
 
   async function getAuthResponse() {
-    const response = await axios.get(
-      "http://localhost:3000/dynamoAuth?email=aaa@aaa.com&password=aaa"
-    );
+    const response = await axios.get("http://localhost:3000/dynamoAuth?email=aaa@aaa.com&password=aaa");
     console.log("Backend response:", response.data);
     setAuthResponse(response.data);
   }
@@ -23,5 +22,10 @@ export function DynamoAuthPage() {
   } else if (!authResponse) {
     message = "User does not exist";
   }
-  return <main>{message}</main>;
+  return (
+    <main>
+      <h2>backend dynamo response</h2>
+      {message}
+    </main>
+  );
 }
