559732227600e6c5dc9ac00663203640961fe648
anyoneout <chrisdafur@gmail.com>
2025-04-16 18:46:19 -0500

    updated getDynamoAuth.test.ts to traditional AAA format. I had used beforeAll as I was getting blocked for hitting the api too quickly but it not longer seems to be an issue

--- backend/src/modules/getDynamoAuth.test.ts
+++ backend/src/modules/getDynamoAuth.test.ts
@@ -1,26 +1,31 @@
 import { getDynamoAuth } from "./getDynamoAuth";
 
-let successResult: boolean;
-let wrongPasswordResult: boolean;
-let noUserResult: boolean;
-
-//beforeAll is being used to deal with multiple api calls in a short span being rejected
-beforeAll(async () => {
-  successResult = await getDynamoAuth("aaa@aaa.com", "aaa");
-  wrongPasswordResult = await getDynamoAuth("aaa@aaa.com", "wrongPassword");
-  noUserResult = await getDynamoAuth("nonUser@email.com", "aaa");
-});
-
 describe("dynamoAuth", () => {
-  it("returns true for correct email and password", () => {
-    expect(successResult).toBe(true);
+  it("returns true for correct email and password", async () => {
+    // ARRANGE
+    const email = "aaa@aaa.com";
+    const password = "aaa";
+
+    // ACT
+    const result = await getDynamoAuth(email, password);
+
+    // ASSERT
+    expect(result).toBe(true);
   });
 
-  it("returns false for an incorrect password", () => {
-    expect(wrongPasswordResult).toBe(false);
+  it("returns false for an incorrect password", async () => {
+    const email = "aaa@aaa.com";
+    const password = "wrongPassword";
+
+    const result = await getDynamoAuth(email, password);
+    expect(result).toBe(false);
   });
 
-  it("returns false for an unknown user", () => {
-    expect(noUserResult).toBe(false);
+  it("returns false for an unknown user", async () => {
+    const email = "nonUser@email.com";
+    const password = "aaa";
+
+    const result = await getDynamoAuth(email, password);
+    expect(result).toBe(false);
   });
 });
