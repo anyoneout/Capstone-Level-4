c8157b33ccdd171e4d70e53e8144763864fdc88e
anyoneout <chrisdafur@gmail.com>
2025-04-29 16:41:44 -0500

    updated TriviaApiResponsePage.tsx

--- frontend/src/Views/TriviaApiResponsePage.tsx
+++ frontend/src/Views/TriviaApiResponsePage.tsx
@@ -1,34 +1,54 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
+import { selectTriviaApiDidMount, selectTriviaApiStatus, selectTriviaApiTrivia } from "../redux/stateSelectors";
+import { useDispatch, useSelector } from "react-redux";
+import { set } from "../redux/store";
 
 export function TriviaApiResponsePage() {
-  const [apiTrivia, setApiTrivia] = useState<string>("");
-  const [apiStatus, setApiStatus] = useState<string>("active");
+  const didMount = useSelector(selectTriviaApiDidMount);
+  const apiTrivia = useSelector(selectTriviaApiTrivia);
+  const apiStatus = useSelector(selectTriviaApiStatus);
+
+  const dispatch = useDispatch();
 
   useEffect(componentDidMount, []);
+  useEffect(componentDidUpdate, [didMount]);
+  useEffect(componentDidUnmount, []);
 
   return (
     <main>
-      <div className="container navbar-width">
-        <h2>Api Trivia</h2>
-        <p>{apiTrivia}</p>
-        <h4>Backend Status</h4>
-        <p>{apiStatus}</p>
-      </div>
+      <h2>Api Trivia</h2>
+      <p>{apiTrivia}</p>
+      <h4>Backend Status</h4>
+      <p>{apiStatus}</p>
     </main>
   );
-
-  function componentDidMount() {
-    getApiResponse();
-  }
 
   async function getApiResponse() {
     try {
       const response = await axios.get("http://localhost:3000/triviaRoute");
       const stringified = JSON.stringify(response.data);
-      setApiTrivia(stringified);
+      dispatch(set.apiTrivia(stringified));
     } catch {
-      setApiStatus("unreachable");
+      dispatch(set.apiStatus("unreachable"));
     }
+  }
+
+  function componentDidMount(): void {
+    dispatch(set.triviaApiDidMount(true));
+    getApiResponse();
+    console.log("The Trivia Api page component has mounted");
+    document.title = "Recipe Deconstructor - Trivia Api Page";
+  }
+
+  function componentDidUpdate(): void {
+    if (didMount) console.log("component had updated");
+  }
+
+  function componentDidUnmount(): () => void {
+    function delayedUnmount(): void {
+      console.log("component has unmounted");
+    }
+    return delayedUnmount;
   }
 }
