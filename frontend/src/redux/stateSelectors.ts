159e1744bdbe1e4366cb364cb188e164814b8612
anyoneout <chrisdafur@gmail.com>
2025-04-28 20:36:13 -0500

    added stateSelectors.ts

---
+++ frontend/src/redux/stateSelectors.ts
@@ -0,0 +1,79 @@
+import { StateVariables } from "./stateVariables";
+
+//didMount
+export function selectAboutDidMount(state: StateVariables) {
+  return state.aboutStateVar.didMount;
+}
+export function selectBfPageDidMount(state: StateVariables) {
+  return state.bfPageStateVar.didMount;
+}
+export function selectExampleDidMount(state: StateVariables) {
+  return state.examplesStateVar.didMount;
+}
+export function selectHomeDidMount(state: StateVariables) {
+  return state.homeStateVar.didMount;
+}
+export function selectOaPageDidMount(state: StateVariables) {
+  return state.oaPageStateVar.didMount;
+}
+export function dynamoAuthDidMount(state: StateVariables) {
+  return state.dynamoAuthStateVar.didMount;
+}
+export function triviaApiDidMount(state: StateVariables) {
+  return state.triviaApiStateVar.didMount;
+}
+export function awsFormsDidMount(state: StateVariables) {
+  return state.awsFormsStateVar.didMount;
+}
+
+//create account
+export function selectCreateEmail(state: StateVariables) {
+  return state.createAccountState.email;
+}
+export function selectCreatePassword(state: StateVariables) {
+  return state.createAccountState.password;
+}
+export function selectCreateName(state: StateVariables) {
+  return state.createAccountState.name;
+}
+export function selectCreatePhone(state: StateVariables) {
+  return state.createAccountState.phone;
+}
+export function selectCreateResponseMessage(state: StateVariables) {
+  return state.createAccountState.responseMessage;
+}
+export function selectCreateReadMessage(state: StateVariables) {
+  return state.createAccountState.readResponse;
+}
+
+//read account
+
+export function selectReadEmail(state: StateVariables) {
+  return state.readAccountState.email;
+}
+export function selectReadResponseMessage(state: StateVariables) {
+  return state.readAccountState.responseMessage;
+}
+
+//update account
+export function selectUpdateEmail(state: StateVariables) {
+  return state.updateAccountState.email;
+}
+export function selectUpdatePassword(state: StateVariables) {
+  return state.updateAccountState.password;
+}
+export function selectUpdateResponseMessage(state: StateVariables) {
+  return state.updateAccountState.responseMessage;
+}
+
+//delete account
+
+export function selectDeleteEmail(state: StateVariables) {
+  return state.deleteAccountState.email;
+}
+export function selectDeletePassword(state: StateVariables) {
+  return state.deleteAccountState.password;
+}
+export function selectDeleteResponseMessage(state: StateVariables) {
+  return state.deleteAccountState.responseMessage;
+}
