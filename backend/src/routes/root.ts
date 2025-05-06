1f8784f91f683a58a69522fb3cf78ba9bafabe27
anyoneout <chrisdafur@gmail.com>
2025-05-06 11:51:09 -0500

    updated root.ts message for lambda backend

--- backend/src/routes/root.ts
+++ backend/src/routes/root.ts
@@ -1,8 +1,5 @@
 import { Request, Response } from "express";
 
 export function root(request: Request, response: Response) {
-  const { url } = request;
-  response.send(`<h1>Hello world!</h1><p>Your root path is ${url}</p`);
-  debugger;
-  // response.send({ message: "what up?!" });
+  response.send(`Lambda server is connected`);
 }
